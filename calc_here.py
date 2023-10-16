596459778c57143f1d6e9fbb39c4b9edb3d0d3f5
RussellBrowe <grigorio-rus@mail.ru>
2023-10-16 13:19:50 +0300

    get a new function percent

--- calc_here.py
+++ calc_here.py
@@ -1,36 +1,50 @@
 def summ(a, b):
-  return a + b
+    return a + b
+
 
 def diff(a, b):
-  return a - b
+    return a - b
+
 
 def multiple(a, b):
-  return a * b
+    return a * b
+
 
 def division(a, b):
-  return a / b
+    return a / b
+
 
 def exp(a, b):
-  return a ** b
+    return a ** b
+
+def percent(a, b):
+  return a * (b / 100)
+
 
 def operation():
-  op = input('Выберите операцию: +(сложение), -(вычитание), *(умножение), /(деление), **(возведение в степень)')
-  return op
+    op = input('Выберите операцию: + (сложение), - (вычитание), * (умножение), / (деление), ** (возведение в степень), \n% (для вычисления процента от числа)')
+    return op
+
 
 def calculator():
-  first_number = int(input('Введите первое число '))
-  second_number = int(input('Введите второе число '))
-  op = operation()
-  if op == '+':
-    print(summ(first_number, second_number))
-  elif op == '-':
-    print(diff(first_number, second_number))
-  elif op == '*':
-    print(multiple(first_number, second_number))
-  elif op == '/':
-    print(division(first_number, second_number))
-  elif op == '**':
-    print(exp(first_number, second_number))
-  else:
-    print('Эту операцию калькулятор не поддерживает')
+    print(f'Учтите, что при вычислении процента второе число это и есть процент который вы хотите вычислить от первого числа')
+
+    first_number = int(input('Введите первое число '))
+    second_number = int(input('Введите второе число '))
+    op = operation()
+    if op == '+':
+        print(summ(first_number, second_number))
+    elif op == '-':
+        print(diff(first_number, second_number))
+    elif op == '*':
+        print(multiple(first_number, second_number))
+    elif op == '/':
+        print(division(first_number, second_number))
+    elif op == '**':
+        print(exp(first_number, second_number))
+    elif op == '%':
+        print(percent(first_number, second_number))
+    else:
+        print('Эту операцию калькулятор не поддерживает')
+
 
