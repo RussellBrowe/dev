990a16466226e77a50662fdde7bb7cd0175899ea
RussellBrowe <grigorio-rus@mail.ru>
2023-10-16 12:00:15 +0300

    for_git created, mainpy renamed to calchere

---
+++ for_git.py
@@ -0,0 +1,10 @@
+def action(var):
+    var = 3
+    print(f'var after changing in function - {var}')
+
+
+example = 5
+print(f'new var in area - {example}')
+
+action(example)
+print(f'check the example after funtion worked - {example}')
