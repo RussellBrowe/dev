6c30a977c040f96335d5f3eb7be41d8bd84ceeaf
RussellBrowe <grigorio-rus@mail.ru>
2023-10-19 10:40:16 +0300

    calc funtion changed: type check added, check division by zero added

--- calc_here.py
+++ calc_here.py
@@ -11,40 +11,50 @@ def multiple(a, b):
 
 
 def division(a, b):
-    return a / b
+    if b != 0:
+        return a / b
+    print(f'Ошибка при делении на ноль')
 
 
 def exp(a, b):
     return a ** b
 
+
 def percent(a, b):
-  return a * (b / 100)
+    return a * (b / 100)
 
 
 def operation():
-    op = input('Выберите операцию: + (сложение), - (вычитание), * (умножение), / (деление), ** (возведение в степень), \n% (для вычисления процента от числа)')
+    op = input(
+        'Выберите операцию: + (сложение), - (вычитание), * (умножение), / (деление), ** (возведение в степень), \n% (для вычисления процента от числа)')
     return op
 
 
 def calculator():
     print(f'Учтите, что при вычислении процента второе число, которое вы вводите, это и есть процент который вы хотите вычислить от первого числа')
 
-    first_number = int(input('Введите первое число '))
-    second_number = int(input('Введите второе число '))
-    op = operation()
-    if op == '+':
-        print(summ(first_number, second_number))
-    elif op == '-':
-        print(diff(first_number, second_number))
-    elif op == '*':
-        print(multiple(first_number, second_number))
-    elif op == '/':
-        print(division(first_number, second_number))
-    elif op == '**':
-        print(exp(first_number, second_number))
-    elif op == '%':
-        print(percent(first_number, second_number))
+    try:
+        first_number = float(input('Введите первое число '))
+        second_number = float(input('Введите второе число '))
+        op = operation()
+    except ValueError:
+        print(f'Невозможно преобразовать строку в число!!')
     else:
-        print('Эту операцию калькулятор не поддерживает')
+        if op == '+':
+            print(summ(first_number, second_number))
+        elif op == '-':
+            print(diff(first_number, second_number))
+        elif op == '*':
+            print(multiple(first_number, second_number))
+        elif op == '/':
+            print(division(first_number, second_number))
+        elif op == '**':
+            print(exp(first_number, second_number))
+        elif op == '%':
+            print(percent(first_number, second_number))
+        else:
+            print('Эту операцию калькулятор не поддерживает')
 
 
+
+calculator()
